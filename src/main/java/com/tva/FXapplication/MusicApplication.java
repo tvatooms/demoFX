a91985c125dffbe9c291cfd8bfa92f9b82d7770b
thomasv <toomy_1999@yahoo.fr>
2025-10-07 10:04:04 +0200

    add submodule

--- src/main/java/com/tva/FXapplication/MusicApplication.java
+++ src/main/java/com/tva/FXapplication/MusicApplication.java
@@ -4,10 +4,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import javafx.stage.Stage;
 import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 
 public class MusicApplication extends Application {
     private ConfigurableApplicationContext applicationContext;
-
+    private static Stage stage;
     @Override
     public void init() {
        applicationContext = new SpringApplicationBuilder(Main.class).run();
@@ -18,9 +23,16 @@ public class MusicApplication extends Application {
         applicationContext.close();
     }
 
+    private Parent createUI()
+    {
+        return new StackPane(new Text("Hello World"));
+    }
 
     @Override
     public void start(Stage primaryStage) {
+         stage = primaryStage;
+         stage.setScene(new Scene(createUI(), 300, 300));
+         stage.show();
     }
 
 }
