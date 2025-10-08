449204ba8dfb98e45ac8dcf0f05adacd46322c48
thomasv <toomy_1999@yahoo.fr>
2025-10-08 15:53:59 +0200

    addicon

--- src/main/java/com/tva/FXapplication/MusicApplication.java
+++ src/main/java/com/tva/FXapplication/MusicApplication.java
@@ -1,21 +1,26 @@
 package com.tva.FXapplication;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.DragEvent;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import java.io.*;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
-import javafx.stage.Stage;
-import javafx.application.Application;
-import javafx.scene.Parent;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.text.Text;
 
 public class MusicApplication extends Application {
     private ConfigurableApplicationContext applicationContext;
-    private static Stage stage;
+
     @Override
     public void init() {
-       applicationContext = new SpringApplicationBuilder(Main.class).run();
+        applicationContext = new SpringApplicationBuilder(Main.class).run();
     }
 
     @Override
@@ -23,16 +28,43 @@ public class MusicApplication extends Application {
         applicationContext.close();
     }
 
-    private Parent createUI()
-    {
-        return new StackPane(new Text("Hello World"));
-    }
-
     @Override
     public void start(Stage primaryStage) {
-         stage = primaryStage;
-         stage.setScene(new Scene(createUI(), 300, 300));
-         stage.show();
+
+        GridPane pane = new GridPane();
+        // InputStream stream = new
+        // FileInputStream("/media/tva/save2/dev/demoFX/FXapplication/resources/add.png");
+        // Image image = new Image(stream);
+        String url =  getClass().getResource("add.png").toString();
+        ImageView imageView = new ImageView(url);
+        pane.add(imageView, 1, 1);
+
+        pane.setOnDragDropped((DragEvent event) -> {
+            Dragboard db = event.getDragboard();
+
+            boolean success = false;
+            if (db.hasFiles()) {
+
+                success = true;
+            }
+            /*
+             * let the source know whether the string was successfully
+             * transferred and used
+             */
+            event.setDropCompleted(success);
+            event.consume();
+        });
+
+        pane.setOnDragOver((DragEvent event) -> {
+            // System.out.println("ICI "+event);
+            event.acceptTransferModes(TransferMode.COPY_OR_MOVE);
+            event.consume();
+        });
+        Scene scene = new Scene(pane, 1024, 800, true);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Partition simplifier");
+
+        primaryStage.show();
     }
 
 }
