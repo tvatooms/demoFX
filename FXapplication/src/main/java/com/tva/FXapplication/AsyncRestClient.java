20d964cefa29a7cc0e1cb5131ec53454bb5915ec
thomasv <toomy_1999@yahoo.fr>
2025-10-14 16:13:40 +0200

    fix webclient

--- FXapplication/src/main/java/com/tva/FXapplication/AsyncRestClient.java
+++ FXapplication/src/main/java/com/tva/FXapplication/AsyncRestClient.java
@@ -1,34 +1,26 @@
 package com.tva.FXapplication;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
 import org.springframework.web.client.RestTemplate;
 
- 
+public class AsyncRestClient {
+	private final WebClient webClient;
 
-@RestController
-public class PartitionClientController {
-
-	private  AsyncRestClient restClient;
-
-	public PartitionClientController() {
-		System.out.println("test");
+	public AsyncRestClient(String uri) {
+		this.webClient = WebClient.builder().baseUrl(uri).build();
 	}
 
-	public PartitionClientController(String uri) {
-		restClient = new AsyncRestClient(uri);
+	public Mono<String> isConnected() {
+		return webClient.get().uri("/").accept(MediaType.APPLICATION_JSON).retrieve().bodyToMono(String.class);	
 	}
-
-	// @GetMapping("/")
-	public void ping() {
-		System.out.println("---->" + restClient.isConnected().block());
-
-	}
-
-}
+};
